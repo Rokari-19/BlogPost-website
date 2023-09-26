85c04060072f4d242aa45352c6dbcbc776b79bd6
rokarioss <oshomahiyelobu1@gmail.com>
2023-09-26 21:55:42 +0100

    mobile view testing

--- app.py
+++ app.py
@@ -1,6 +1,45 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
+
+
+
+POSTS = [
+    {
+        'id':'1',
+        'class':'food',
+        'content':'lorem ipsum',
+        'header':'lorem ipsuck my titties'
+    },
+
+    {
+        'id':'2',
+        'class':'food',
+        'content':'lorem ipsum',
+        'header':'lorem ipsuck my titties'
+    },
+
+    {
+        'id':'3',
+        'class':'food',
+        'content':'lorem ipsum',
+        'header':'lorem ipsuck my titties'
+    },
+
+    {
+        'id':'4',
+        'class':'food',
+        'content':'lorem ipsum',
+        'header':'lorem ipsuck my titties'
+    },
+
+    {
+        'id':'5',
+        'class':'food',
+        'content':'lorem ipsum',
+        'header':'lorem ipsuck my titties'
+    }
+]
 
 @app.route('/')
 def home():
@@ -8,11 +47,19 @@ def home():
 
 @app.route('/welcome')
 def welcome():
-    return render_template('welcome.html')
+    return render_template('welcome.html', data=POSTS)
 
 @app.route('/signin')
 def signin():
     return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/data')
+def get_post():
+    return jsonify(POSTS)
 
 if __name__ == "__main__":
     app.run(debug=True)
