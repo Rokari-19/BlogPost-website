7c268fa49b03afc82a900e64ef8c44603fd6c092
rokarioss <oshomahiyelobu1@gmail.com>
2023-09-27 15:42:54 +0100

    losing my mind

--- static/js/functions.js
+++ static/js/functions.js
@@ -2,6 +2,7 @@ function collectFormData() {
     let formData = document.getElementsByClassName('js-form');
     let userName = document.getElementsByClassName('e1Entry');
     console.log(userName)
+    alert('hello' + userName)
 
 
 
