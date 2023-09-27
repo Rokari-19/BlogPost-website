e8c3f7f3a162643f45abdf8c47039981eb06ea13
rokarioss <oshomahiyelobu1@gmail.com>
2023-09-27 13:32:11 +0100

    changed some things hehe

---
+++ static/js/functions.js
@@ -0,0 +1,10 @@
+function collectFormData() {
+    let formData = document.getElementsByClassName('js-form');
+    let userName = document.getElementsByClassName('e1Entry');
+    console.log(userName)
+
+
+
+
+}
+
